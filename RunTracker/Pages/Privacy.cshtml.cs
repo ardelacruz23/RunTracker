b9e988621b45023e7912f34d6c6d074756f6bcd2
Angelica Delacruz <112988683+ardelacruz23@users.noreply.github.com>
2023-02-03 16:18:22 -0600

    Add project files.

---
+++ RunTracker/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RunTracker.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
