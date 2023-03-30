c2f55acf2437f03020a7e1b46e8eaf22d0161731
ardelacruz23 <ardelacruz99@gmail.com>
2023-03-30 16:16:00 -0500

    Startup.cs Login.cshtml bg image

---
+++ RunTracker/Startup.cs
@@ -0,0 +1,18 @@
+﻿namespace RunTracker
+{
+	public class Startup
+	{
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+		}
+	}
+}
