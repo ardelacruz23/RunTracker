3b8da8b47341d0fc25c2a1e1635d612ad8385f40
ardelacruz23 <ardelacruz99@gmail.com>
2023-03-31 16:41:44 -0500

    hi

--- RunTracker/Models/User.cs
+++ RunTracker/Models/User.cs
@@ -4,13 +4,18 @@ namespace RunTracker.Models
 {
     public class User
     {
+        [Required (ErrorMessage = "Please enter a valid username.")]
         public int UserId { get; set; }
+        [Required (ErrorMessage = "Please enter a valid email.")]
         public string Email { get; set; }
+        [Required (ErrorMessage = "Please enter a valid first name.")]
         public string FirstName { get; set; }
-        public string LastName { get; set; }
+		[Required(ErrorMessage = "Please enter a valid last name.")]
+		public string LastName { get; set; }
         public TimestampAttribute LastLogin { get; set; }
         public string AvatarURL { get; set; }
-        public string Salt { get; set; }    
+        public string Salt { get; set; }
+        [Required (ErrorMessage = "Please enter a valid password.")]
         public string PasswordHash { get; set; }
 
     }
